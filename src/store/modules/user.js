1f1ed45880fb5b41d37fd62a47b135cd27ed3e8c
moxiaoyu12138 <1620889647@qq.com>
2022-02-25 12:39:41 +0800

    封装请求

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,28 @@
+import { setToken, getToken, removeToken } from '@/utils/auth'
+const state = {
+  token: getToken // 初始化vuex的时候就从缓存中读取token
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  }
+}
+const actions = {
+  async login(context, data) {
+    const result = await this.login(data)
+    if (result.data.success) {
+      context.commit('setToken', result.data.data)
+    }
+  }
+}
 export default {
   nemespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
